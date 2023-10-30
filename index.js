0355dc791db76217cb7a37f969df5ba2ba327866
SkyAisha <aisha.adia@sky.uk>
2023-10-30 15:18:29 +0000

    workshop taks completed url is logged

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
+async function getImages(query) {
+  try {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=cats&limit=25&offset=0&rating=g&lang=en`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const imageURL = data.data[0].url;
+    return imageURL;
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+getImages("cats").then((imageURL) => {
+  console.log("GIF Image URL:", imageURL);
+});
